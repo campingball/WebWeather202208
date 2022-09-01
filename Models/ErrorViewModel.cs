9793b7adf5f6221323c08356ae9e33347e296e35
campingball <ktrtmd1975@yahoo.co.jp>
2022-09-01 16:53:00 +0900

    プロジェクト ファイルを追加します。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebWeather202208.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/WeatherViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebWeather202208.Models
+{
+    public class WeatherViewModel
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Lat { get; set; }
+        public string Lon { get; set; }
+        public string Description { get; set; }
+        public string Humiditu { get; set; }
+    }
+}
